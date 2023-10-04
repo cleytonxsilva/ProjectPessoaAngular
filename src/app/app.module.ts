daeefa3d517bf3be714081da18b2ff69d5ef95d1
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-10-03 21:30:53 -0300

    add arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PessoalistComponent } from './app/pessoa/pessoalist/pessoalist.component';
+import { IndexComponent } from './app/layout/index/index.component';
+import { MenusuperiorComponent } from './app/layout/menusuperior/menusuperior.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PessoalistComponent,
+    IndexComponent,
+    MenusuperiorComponent
   ],
   imports: [
     BrowserModule,
