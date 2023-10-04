18087c30a51723dc87be41aaa3d61f59521d971c
Cleyton da Silva Pires <cleytonxsilva@outlook.com>
2023-10-04 00:19:40 -0300

    add style

--- src/app/app/pessoa/pessoalist/pessoalist.component.ts
+++ src/app/app/pessoa/pessoalist/pessoalist.component.ts
@@ -11,43 +11,43 @@ export class PessoalistComponent {
   constructor(){
     let pessoa1: Pessoa = new Pessoa();
     pessoa1.nome = "Cleyton";
-    pessoa1.idade = 26;
+    pessoa1.idade = 100;
 
     let pessoa2: Pessoa = new Pessoa();
     pessoa2.nome = "Andre";
-    pessoa2.idade = 21;
+    pessoa2.idade = 99;
 
     let pessoa3: Pessoa = new Pessoa();
     pessoa3.nome = "Andrei";
-    pessoa3.idade = 20;
+    pessoa3.idade = 98;
 
     let pessoa4: Pessoa = new Pessoa();
     pessoa4.nome = "Fred";
-    pessoa4.idade = 21;
+    pessoa4.idade = 97;
 
     let pessoa5: Pessoa = new Pessoa();
     pessoa5.nome = "Luis";
-    pessoa5.idade = 27;
+    pessoa5.idade = 96;
 
     let pessoa6: Pessoa = new Pessoa();
     pessoa6.nome = "CleytonCópia";
-    pessoa6.idade = 26;
+    pessoa6.idade = 5;
 
     let pessoa7: Pessoa = new Pessoa();
     pessoa7.nome = "AndreCópia";
-    pessoa7.idade = 21;
+    pessoa7.idade = 4;
 
     let pessoa8: Pessoa = new Pessoa();
     pessoa8.nome = "AndreiCópia";
-    pessoa8.idade = 20;
+    pessoa8.idade = 3;
 
     let pessoa9: Pessoa = new Pessoa();
     pessoa9.nome = "FredCópia";
-    pessoa9.idade = 21;
+    pessoa9.idade = 2;
 
     let pessoa10: Pessoa = new Pessoa();
     pessoa10.nome = "LuisCópia";
-    pessoa10.idade = 27;
+    pessoa10.idade = 1;
 
 
     this.lista.push(pessoa1);
